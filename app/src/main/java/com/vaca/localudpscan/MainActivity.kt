babdbd3567f52ef7a1b7d5ee1bac31cfb6256fa9
vaca22 <wsmslgh@gmail.com>
2021-12-26 20:49:50 +0800

    dtyjsadfg

--- app/src/main/java/com/vaca/localudpscan/MainActivity.kt
+++ app/src/main/java/com/vaca/localudpscan/MainActivity.kt
@@ -2,10 +2,13 @@ package com.vaca.localudpscan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vaca.localudpscan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
